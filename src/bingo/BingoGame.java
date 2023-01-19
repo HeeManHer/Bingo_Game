7cffe571452f87ca454bdce46cce12db391465b8
HeeMan Her <heeman7211@gmail.com>
2023-01-19 16:33:15 +0900

    빙고 게임 진행

--- src/bingo/BingoGame.java
+++ src/bingo/BingoGame.java
@@ -18,7 +18,7 @@ public class BingoGame {
 			System.out.println("1. 혼자서");
 			System.out.println("2. 둘이서");
 			int no = sc.nextInt();
-
+String name;
 			switch (no) {
 			case 0:
 				System.out.println("게임을 종료합니다.");
@@ -26,9 +26,12 @@ public class BingoGame {
 
 			case 1:
 				System.out.println("컴퓨터랑 대전합니다.");
-				play[0] = new Player();
+				System.out.println("플레이어1의 이름을 입력하세요 : ");
+				sc.nextLine();
+				name = sc.nextLine();
+				play[0] = new Player(name);
 				play[0].Make();
-
+				
 				play[1] = new Com();
 				play[1].Make();
 
@@ -36,10 +39,15 @@ public class BingoGame {
 
 			case 2:
 				System.out.println("플레이어끼리 대전합니다.");
-				play[0] = new Player("p1");
+				System.out.println("플레이어1의 이름을 입력하세요 : ");
+				sc.nextLine();
+				name = sc.nextLine();
+				play[0] = new Player(name);
 				play[0].Make();
-
-				play[1] = new Player("p2");
+				
+				System.out.println("플레이어2의 이름을 입력하세요 : ");
+				name = sc.nextLine();
+				play[1] = new Player(name);
 				play[1].Make();
 
 				break;
@@ -49,27 +57,7 @@ public class BingoGame {
 			}
 		}
 
-		int[] num = { 0, 0 };
-
-		do {
-			play[0].Print();
-			play[1].Print();
-			num[0] = play[0].Turn(num[1]);
-			System.out.println(play[0].getName() + "가 선택한 숫자는 " + num[0] + "입니다.\n");
-			play[1].check(num[0]);
-
-			if (play[0].getBingoCount() >= 3 || play[1].getBingoCount() >= 3) {
-				break;
-			}
-			
-			play[0].Print();
-			play[1].Print();
-
-			num[1] = play[1].Turn(num[0]);
-			System.out.println(play[1].getName() + "이 선택한 숫자는 " + num[1] + "입니다.\n");
-			play[0].check(num[1]);
-
-		} while (play[0].getBingoCount() < 3 || play[1].getBingoCount() < 3);
+		gamePlay();		
 
 		System.out.println("B! I! N! G! O!");
 		System.out.println("게임 종료!!");
@@ -86,6 +74,40 @@ public class BingoGame {
 		} else {
 			System.out.println("무승부 입니다.");
 		}
+	}
+	
+	public void gamePlay() {
+		
+		int[] num = new int[play.length];
+		
+		while (true) {
+			for (int i = 0; i < play.length; i++) {
+				if (play[i] != null) {
+
+					for (int j = 0; j < play.length; j++) {
+						if (!(play[j] instanceof Com) && play[j] != null) {
+							play[j].Print();
+						}
+					}
+
+					num[i] = play[i].Turn();
+					System.out.println(play[i].getName() + "가 선택한 숫자는 " + num[i] + "입니다.\n");
+
+					for (int k = 0; k < play.length; k++) {
+						if (k != i) {
+							play[k].check(num[i]);
+						}
+					}
+
+					for (int a = 0; a < play.length; a++) {
+						if (play[a].getBingoCount() >= 3) {
+							return;
+						}
+					}
+				}
+			}
+		}
+		
 	}
 
 }
